4c56c05ce1771cb740fc4b5998c5e8b4af20e430
Noah Torrance <noahtorrance27@gmail.com>
2023-05-24 19:39:34 +0100

    Update Waybar

--- users/noah/desktop.nix
+++ users/noah/desktop.nix
@@ -120,4 +120,15 @@
   programs.librewolf = {
     enable = true;
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      waybar = super.waybar.overrideAttrs (oldAttrs: {
+        preBuild = ''
+	  sed -i -e 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' ../src/modules/wlr/workspace_manager.cpp
+        '';
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+      });
+    })
+  ];
 }

cab824b8d5e30d10d97dfe9475331d427814dec4
Noah Torrance <noahtorrance27@gmail.com>
2023-05-28 17:36:52 +0100

    Update Alacritty config

--- users/noah/desktop.nix
+++ users/noah/desktop.nix
@@ -158,10 +158,6 @@
     enable = true;
     settings = {
       env.TERM = "xterm-256color";
-      window.padding = {
-        x = 2;
-        y = 2;
-      };
     };
   };
   programs.librewolf = {

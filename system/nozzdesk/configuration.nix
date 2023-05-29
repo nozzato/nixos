8c91652f966a2d0a1d77dcdb61f8df4ec4340fbc
Noah Torrance <noahtorrance27@gmail.com>
2023-05-29 23:58:15 +0100

    Mount windows partition

--- system/nozzdesk/configuration.nix
+++ system/nozzdesk/configuration.nix
@@ -33,15 +33,20 @@
 
   # File systems
   fileSystems = {
-    "/media/store" = {
-      device = "nozzbox:/box/store";
-      fsType = "nfs";
-      options = [ "noauto" "user" "_netdev" "bg" ];
-    };
     "/media/share" = {
       label = "share";
       fsType = "ntfs";
       options = [ "uid=1000" "gid=1000" "dmask=022" "fmask=033" ];
+    };
+    "/media/windows" = {
+      label = "windows";
+      fsType = "ntfs";
+      options = [ "uid=1000" "gid=1000" "dmask=022" "fmask=033" ];
+    };
+    "/media/store" = {
+      device = "nozzbox:/box/store";
+      fsType = "nfs";
+      options = [ "noauto" "user" "_netdev" "bg" ];
     };
   };
   swapDevices = [

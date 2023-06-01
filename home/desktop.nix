9699a4ce3aa469c3dd5089731426f246065a4ddd
Noah Torrance <noahtorrance27@gmail.com>
2023-06-01 20:09:37 +0100

    Fix LinkLibreWolf activation

--- home/desktop.nix
+++ home/desktop.nix
@@ -198,7 +198,7 @@
     };
   };
   home.activation.linkLibreWolf = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD ln -s $VERBOSE_ARG \
+    $DRY_RUN_CMD ln -fs $VERBOSE_ARG \
         ${config.home.homeDirectory}/.mozilla/native-messaging-hosts \
         ${config.home.homeDirectory}/.librewolf/native-messaging-hosts
   '';

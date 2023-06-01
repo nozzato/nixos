5302ed59ea4dd2c2a43c8af0ff8c9adb8acb8603
Noah Torrance <noahtorrance27@gmail.com>
2023-06-01 13:20:26 +0100

    Update flake

--- bin/rgb-toggle/flake.nix
+++ bin/rgb-toggle/flake.nix
@@ -5,7 +5,6 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
       my-name = "rgb-toggle";
-      my-buildInputs = with pkgs; [ openrgb ];
       my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./rgb-toggle.sh)).overrideAttrs(old: {
         buildCommand = "${old.buildCommand}\n patchShebangs $out";
       });
@@ -13,7 +12,7 @@
       defaultPackage = packages.rgb-toggle;
       packages.rgb-toggle = pkgs.symlinkJoin {
         name = my-name;
-        paths = [ my-script ] ++ my-buildInputs;
+        paths = [ my-script ];
         buildInputs = [ pkgs.makeWrapper ];
         postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
       };
--- bin/rsync-gnu/flake.nix
+++ bin/rsync-gnu/flake.nix
@@ -5,7 +5,6 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
       my-name = "rsync-gnu";
-      my-buildInputs = with pkgs; [ rsync ];
       my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./rsync-gnu.sh)).overrideAttrs(old: {
         buildCommand = "${old.buildCommand}\n patchShebangs $out";
       });
@@ -13,7 +12,7 @@
       defaultPackage = packages.rsync-gnu;
       packages.rsync-gnu = pkgs.symlinkJoin {
         name = my-name;
-        paths = [ my-script ] ++ my-buildInputs;
+        paths = [ my-script ];
         buildInputs = [ pkgs.makeWrapper ];
         postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
       };

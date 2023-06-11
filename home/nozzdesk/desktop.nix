d548984fc7d5c6850db8259d32401557cd1d1f84
Noah Torrance <noahtorrance27@gmail.com>
2023-06-11 09:17:38 +0100

    Fix monitor name

--- home/nozzdesk/desktop.nix
+++ home/nozzdesk/desktop.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }: {
   wayland.windowManager.hyprland = {
     extraConfig = ''
-      monitor = DP-1, 1920x1080, 0x0, 1
+      monitor = HDMI-A-1, 1920x1080, 0x0, 1
     '';
   };
   programs.waybar = {

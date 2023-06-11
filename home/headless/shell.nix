9086784f6b2a8b2b8d6af173fcc8da9c331997fd
Noah Torrance <noahtorrance27@gmail.com>
2023-06-11 15:37:49 +0100

    Fix flush alias

--- home/headless/shell.nix
+++ home/headless/shell.nix
@@ -15,7 +15,7 @@
       cp = "cp -vir";
       df = "df -h";
       diff = "diff --color=auto";
-      flush = "swapoff -a && swapon -a";
+      flush = "swapoff -a && sudo swapon -a";
       grep = "rg -S";
       history = "history -i";
       hyprpicker = "hyprpicker -ar";

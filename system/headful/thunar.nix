e2941135560103e6ce95bda5fcd07db312f5f637
Noah Torrance <noahtorrance27@gmail.com>
2023-06-11 22:08:36 +0100

    Hide tank from Thunar

--- system/headful/thunar.nix
+++ system/headful/thunar.nix
@@ -10,4 +10,7 @@
   services.gvfs.enable = true;
   services.tumbler.enable = true;
   programs.file-roller.enable = true;
+  services.udev.extraRules = ''
+    SUBSYSTEM=="block", ENV{ID_FS_LABEL}=="tank", ENV{UDISKS_IGNORE}="1"
+  '';
 }

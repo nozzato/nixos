1c9898082a0fb8e0e5de5797bdafa3f4024f8ff4
Noah Torrance <noahtorrance27@gmail.com>
2023-06-20 22:38:36 +0100

    Add monitors

--- home/nozzlap/desktop.nix
+++ home/nozzlap/desktop.nix
@@ -2,7 +2,9 @@
   wayland.windowManager.hyprland = {
     nvidiaPatches = true;
     extraConfig = ''
-      monitor = eDP-1, 1920x1080, 0x0, 1
+      monitor = DP-2, 1920x1080, 0x0, 1
+      monitor = DP-3, 1920x1080, 1920x0, 1
+      monitor = eDP-1, 1920x1080, 3840x0, 1
     '';
   };
   programs.waybar = {

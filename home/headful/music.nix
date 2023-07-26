6628958f7186c32b8aa61132bfeebf1a7c276fe0
Noah Torrance <noahtorrance27@gmail.com>
2023-07-27 00:31:07 +0100

    Unshare MPD database file

--- home/headful/music.nix
+++ home/headful/music.nix
@@ -10,8 +10,7 @@
   services.mpd = {
     enable = true;
     musicDirectory = "${config.home.homeDirectory}/audio/music";
-    dbFile = "${config.home.homeDirectory}/audio/music/tag_cache";
-    playlistDirectory = "${config.home.homeDirectory}/audio/music/playlists";
+    playlistDirectory = "${config.home.homeDirectory}/audio/music";
     extraConfig = ''
       auto_update "yes"
       restore_paused "yes"

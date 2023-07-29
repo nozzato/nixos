7e2c6b622beb6cd19658ba7e063211aff798d52c
Noah Torrance <noahtorrance27@gmail.com>
2023-07-29 11:58:19 +0100

    Remove unused CUDA toolkit

--- system/nozlap/graphics.nix
+++ system/nozlap/graphics.nix
@@ -8,9 +8,6 @@
     driSupport32Bit = true;
   };
 
-  environment.systemPackages = with pkgs; [
-    cudatoolkit
-  ];
   systemd.services.nvidia-control-devices = {
     wantedBy = [ "multi-user.target" ];
     serviceConfig.ExecStart = "${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi";

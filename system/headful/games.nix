0a64ad35dd8bb2434a821f55cc7190e232d9e4bc
Noah Torrance <noahtorrance27@gmail.com>
2023-08-12 14:25:47 +0100

    Add RetroArch

--- system/headful/games.nix
+++ system/headful/games.nix
@@ -1,3 +1,12 @@
 { config, lib, pkgs, ... }: {
   programs.steam.enable = true;
+
+  services.xserver.desktopManager.retroarch.enable = true;
+  environment.systemPackages = with pkgs; [
+    (retroarch.override {
+      cores = with libretro; [
+        pcsx2
+      ];
+    })
+  ];
 }

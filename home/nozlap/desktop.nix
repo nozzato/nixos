d4826fe227d1a4802e2051c2cf0ce14011b22208
Noah Torrance <noahtorrance27@gmail.com>
2023-09-13 23:38:09 +0100

    Update tripple monitor preset

--- home/nozlap/desktop.nix
+++ home/nozlap/desktop.nix
@@ -2,9 +2,9 @@
   home.file."${config.xdg.configHome}/hypr/hyprland.conf".text = ''
     monitor = eDP-1, 1920x1080, 0x0, 1
 
-    #monitor = DP-2, 1920x1080, 0x0, 1, transform, 1
-    #monitor = DP-3, 1920x1080, 1080x420, 1
-    #monitor = eDP-1, 1920x1080, 3000x420, 1
+    #monitor = DP-2, 1920x1080, 0x0, 1
+    #monitor = DP-3, 1920x1080, 1920x0, 1
+    #monitor = eDP-1, 1920x1080, 3840x0, 1
     #workspace = 9, monitor:DP-2
     #workspace = 10, monitor:eDP-1
 

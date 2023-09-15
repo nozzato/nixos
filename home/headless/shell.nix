dc483473de29985a89b115db68d51ea8231f5d54
Noah Torrance <noahtorrance27@gmail.com>
2023-09-15 20:12:58 +0100

    au chocolat

--- home/headless/shell.nix
+++ home/headless/shell.nix
@@ -5,9 +5,6 @@
 
   programs.fish = {
     enable = true;
-    plugins = {
-
-    };
     functions = {
       __fish_command_not_found_handler = {
         body = "__fish_default_command_not_found_handler $argv[1]";

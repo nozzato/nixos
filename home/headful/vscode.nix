1027da0862fd18a67034e62c175221bdc5b81070
Noah Torrance <noahtorrance27@gmail.com>
2023-09-16 20:29:54 +0100

    Add Vue extensions

--- home/headful/vscode.nix
+++ home/headful/vscode.nix
@@ -3,16 +3,34 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
+      # Formatter
       esbenp.prettier-vscode
+
+      # Web
+      bradlc.vscode-tailwindcss
 
       # Nix
       jnoortheen.nix-ide
       mkhl.direnv
-
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       # Web
-      bradlc.vscode-tailwindcss
+      {
+        publisher = "Vue";
+        name = "volar";
+        version = "1.8.11";
+        sha256 = "sha256-Y3vhArpoXCGHs/A47716XBkbtGKv6koepZtm/ukY7k8=";
+      }
+      {
+        publisher = "Vue";
+        name = "vscode-typescript-vue-plugin";
+        version = "1.8.11";
+        sha256 = "sha256-ubvSDztn0cYNMyh+Cm7l48N6wn1Zt6LWpnw869idg7o=";
+      }
     ];
     userSettings = {
+      css = {
+        validate = false;
+      };
       diffEditor = {
         renderSideBySide = false;
       };
@@ -23,6 +41,9 @@
         lineDecorationsWidth = 0;
         minimap = {
           enabled = false;
+        };
+        quickSuggestions = {
+          strings = true;
         };
         roundedSelection = false;
         scrollbar = {
@@ -37,6 +58,12 @@
       nix = {
         enableLanguageServer = true;
         serverPath = "nil";
+      };
+      tailwindCSS = {
+        includeLanguages = {
+          vue = "html";
+          vue-html = "html";
+        };
       };
       update = {
         mode = "none";

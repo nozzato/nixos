e39635ad18a458fac6a4ba562bd25ba0c55fe0b4
Noah Torrance <noahtorrance27@gmail.com>
2024-05-09 21:04:43 +0100

    Update samba share

--- system/client.nix
+++ system/client.nix
@@ -52,7 +52,7 @@
     "/media/nozbox" = {
       device = "//192.168.1.6/noah";
       fsType = "cifs";
-      options = ["x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,credentials=/etc/nixos/smb-secrets,uid=1000,gid=100,dir_mode=0700,file_mode=0700"];
+      options = [ "x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,credentials=/etc/nixos/smb-nozbox-secrets,uid=1000,gid=100,dir_mode=0700,file_mode=0700" ];
     };
   };
 
@@ -90,6 +90,9 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
 
   # Printing
   services.printing.enable = true;

3cd1a7dc573854c44dc72d846613417203b2159b
Noah Torrance <noahtorrance27@gmail.com>
2024-05-11 01:39:58 +0100

    Add extra locale settings

--- system/default.nix
+++ system/default.nix
@@ -72,8 +72,15 @@
   i18n = {
     defaultLocale = "en_GB.UTF-8";
     extraLocaleSettings = {
-      LC_NUMERIC = "C.UTF-8";
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
       LC_NAME = "C.UTF-8";
+      LC_NUMERIC = "C.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
     };
   };
   time.timeZone = "Europe/London";

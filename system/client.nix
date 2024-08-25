9319c5efdaddd77c0ac05d45d971fc215417b960
Noah Torrance <noahtorrance27@gmail.com>
2024-08-25 20:24:38 +0100

    Add Plymouth boot splash screen

--- system/client.nix
+++ system/client.nix
@@ -1,4 +1,24 @@
 { lib, config, pkgs, ... }: {
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "breeze";
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+  };
+
   fileSystems = {
     "/media/windows" = {
       label = "windows";

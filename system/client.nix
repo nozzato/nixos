eeb81ea06ba8cbe512c11f264e05c9ef07f94266
Noah Torrance <noahtorrance27@gmail.com>
2024-09-25 21:56:20 +0100

    Make boot completly silent

--- system/client.nix
+++ system/client.nix
@@ -8,12 +8,7 @@
     initrd.verbose = false;
     kernelParams = [
       "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
+      "udev.log_level=3"
     ];
   };
 
